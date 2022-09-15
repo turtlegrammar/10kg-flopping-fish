e7eecefb26c341303e280eec4e80eaf4a2c833d6
Joseph DiSalvo <jdisalvo@projetech.com>
2022-09-15 12:02:35 -0400

    fiiiiish

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Note: to get this working with coder, you have to manually `chmod +x install.sh` inside coder after you create the workspace for the first time.
+# chmod'ing survives workspace rebuilds. I'm not sure how to make this automatically executable from the start.
 
 cp personalize ~/personalize
 chmod +x ~/personalize
-sh personalize
+sh ~/personalize
